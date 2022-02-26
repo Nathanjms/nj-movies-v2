a499560e7d83dacc260e12331d391202f749d600
Nathan James <nathanjms@outlook.com>
2022-02-26 14:18:06 +0000

    Optimise css

--- src/components/Global/Navbar.tsx
+++ src/components/Global/Navbar.tsx
@@ -1,26 +1,21 @@
 import React, { ReactElement, useState } from "react";
-import { FaBars, FaSignInAlt, FaSignOutAlt } from "react-icons/fa";
+import { FaBars, FaDoorOpen, FaSignInAlt } from "react-icons/fa";
 import navItems from "./NavItems";
 import { NavLink } from "react-router-dom";
 import "../../css/NavBar.css";
+import { Button } from "react-bootstrap";
 
 const NavBar: React.FC<{}> = (): ReactElement => {
   const [expanded, setExpanded] = useState(false);
 
   const MobileToggleBtn = (): ReactElement => {
     return (
-      <li onClick={() => setExpanded(!expanded)}>
-        {" "}
-        <button
-          className={
-            expanded
-              ? "btn button-shape mobile-button btn-clicked"
-              : "btn button-shape mobile-button"
-          }
-        >
-          <FaBars />
-        </button>
-      </li>
+      <Button
+        onClick={() => setExpanded(!expanded)}
+        className="btn navBtn mobileBtn"
+      >
+        <FaBars />
+      </Button>
     );
   };
 
@@ -28,29 +23,37 @@ const NavBar: React.FC<{}> = (): ReactElement => {
     <header className="navBar-container">
       <nav className="navBar">
         <ul
-          className={!expanded ? "webNav links hide-nav" : "webNav links fadeIn"}
+          className={"webNav links" + (expanded ? " fadeIn" : " hide-nav")}
         >
           {navItems.map((element, id) => {
             return (
               <NavLink
-                className="button-shape nav-link"
+                className="navBtn nav-link"
                 to={element.link}
                 key={id}
-                onClick={() => (setExpanded(false))}
+                onClick={() => setExpanded(false)}
               >
                 {element.icon}
-                <span>{element.text}</span>
+                <span className="nav-text">{element.text}</span>
               </NavLink>
             );
           })}
-          {/*} TODO: Dynamic signin/out buttons: */}
-          <NavLink className="button-shape nav-link" to="/signin">
+          {/*} TODO: Dynamic signin/out buttons based on auth state: */}
+          <NavLink
+            onClick={() => setExpanded(false)}
+            className="navBtn nav-link"
+            to="/signin"
+          >
             <FaSignInAlt />
-            <span>Sign In</span>
+            <span className="nav-text">Sign In</span>
           </NavLink>
-          <NavLink className="button-shape nav-link" to="/signout">
-            <FaSignOutAlt />
-            <span>Sign Out</span>
+          <NavLink
+            onClick={() => setExpanded(false)}
+            className="navBtn nav-link"
+            to="/signout"
+          >
+            <FaDoorOpen />
+            <span className="nav-text">Sign Out</span>
           </NavLink>
         </ul>
         <MobileToggleBtn />
