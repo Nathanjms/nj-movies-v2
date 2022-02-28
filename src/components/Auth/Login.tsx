b9b42d0b566219f49a5f5ef2d21e23e10351a0b2
Nathan James <nathanjms@outlook.com>
2022-02-28 21:52:06 +0000

    Logout method

--- src/components/Auth/Login.tsx
+++ src/components/Auth/Login.tsx
@@ -1,6 +1,14 @@
-import React, { useContext, useState } from "react";
-import { Form, Button, Card, Container, Alert } from "react-bootstrap";
-import { Link, useNavigate } from "react-router-dom";
+import React, { ReactElement, useContext, useState } from "react";
+import {
+  Form,
+  Button,
+  Card,
+  Container,
+  Alert,
+  Row,
+  Col,
+} from "react-bootstrap";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import axios from "axios";
 import { baseURL } from "../Global/apiCommunication";
 import { AuthContext } from "./AuthContext";
@@ -9,9 +17,21 @@ export default function Login() {
   const [email, setEmail] = useState<string>("");
   const [password, setPassword] = useState<string>("");
   const [error, setError] = useState("");
+  const [showAlert, setShowAlert] = useState(true);
   const [loading, setLoading] = useState(false);
   const { setToken } = useContext(AuthContext);
   const navigate = useNavigate();
+  const { state } = useLocation() as any;
+
+  const AlertMessage = (): ReactElement | null => {
+    setShowAlert(true);
+    let message = state?.message;
+    let type = state?.type ? state.type : "info";
+    if (!message) {
+      return null;
+    }
+    return <Alert variant={type} dismissible={true} onClose={() => setShowAlert(false)}>{message}</Alert>;
+  };
 
   async function login(email: string, password: string) {
     setLoading(true);
@@ -51,58 +71,67 @@ export default function Login() {
   }
   return (
     <Container
-      className="d-flex align-items-center justify-content-center"
+      className="justify-content-center"
       style={{ minHeight: "100vh" }}
       id="login"
     >
-      <div className="w-100" style={{ maxWidth: "400px" }}>
-        <Card className="text-black">
-          <Card.Body>
-            <h2 className="text-center mb-4">Log In</h2>
-            {error && <Alert variant="danger">{error}</Alert>}
-            <Form onSubmit={handleSubmit}>
-              <Form.Group id="email">
-                <Form.Label>Email</Form.Label>
-                <Form.Control
-                  className="mb-3"
-                  name="email"
-                  type="email"
-                  onChange={(e) => setEmail(e.target.value)}
-                  required
-                />
-              </Form.Group>
-              <Form.Group id="password">
-                <Form.Label>Password</Form.Label>
-                <Form.Control
-                  className="mb-3"
-                  name="password"
-                  onChange={(e) => setPassword(e.target.value)}
-                  type="password"
-                  required
-                />
-              </Form.Group>
-              <Button disabled={loading} className="w-100" type="submit">
-                Log In
-              </Button>
-            </Form>
-            {loading && (
-              <div className="col-lg-12 mt-4">
-                <p>Connecting to API...</p>
-              </div>
-            )}
-          </Card.Body>
-        </Card>
-        <div className="w-100 text-center mt-2">
-          <p>To access the Movies section, you must be signed in.</p>
-          <p>
-            Don't have an account? <Link to="/demo">Click here</Link> to demo
-            the Movies section.
-          </p>
-          <p>
-            <a href="https://nathanjms.co.uk">Go to main site.</a>
-          </p>
-        </div>
-      </div>
+      <Row className="pt-3">
+        <Col xs={12}>
+          {showAlert && <AlertMessage />}
+        </Col>
+      </Row>
+      <Row className="align-items-center">
+        <Col xs={12}>
+          <div className="w-100" style={{ maxWidth: "400px", margin: "auto" }}>
+            <Card className="text-black">
+              <Card.Body>
+                <h2 className="text-center mb-4">Log In</h2>
+                {error && <Alert variant="danger">{error}</Alert>}
+                <Form onSubmit={handleSubmit}>
+                  <Form.Group id="email">
+                    <Form.Label>Email</Form.Label>
+                    <Form.Control
+                      className="mb-3"
+                      name="email"
+                      type="email"
+                      onChange={(e) => setEmail(e.target.value)}
+                      required
+                    />
+                  </Form.Group>
+                  <Form.Group id="password">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control
+                      className="mb-3"
+                      name="password"
+                      onChange={(e) => setPassword(e.target.value)}
+                      type="password"
+                      required
+                    />
+                  </Form.Group>
+                  <Button disabled={loading} className="w-100" type="submit">
+                    Log In
+                  </Button>
+                </Form>
+                {loading && (
+                  <div className="col-lg-12 mt-4">
+                    <p>Connecting to API...</p>
+                  </div>
+                )}
+              </Card.Body>
+            </Card>
+            <div className="w-100 text-center mt-2">
+              <p>To access the Movies section, you must be signed in.</p>
+              <p>
+                Don't have an account? <Link to="/demo">Click here</Link> to
+                demo the Movies section.
+              </p>
+              <p>
+                <a href="https://nathanjms.co.uk">Go to main site.</a>
+              </p>
+            </div>
+          </div>
+        </Col>
+      </Row>
     </Container>
   );
 }
