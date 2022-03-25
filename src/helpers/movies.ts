666e33ad81cbe547747d8e58fd204b605bb00eea
Nathan James <nathanjms@outlook.com>
2022-03-25 22:29:44 +0000

    Types

--- src/helpers/movies.ts
+++ src/helpers/movies.ts
@@ -4,3 +4,27 @@ export const perPage = () => {
     return 6;
   return 4;
 };
+
+interface validMovieOrderColumnObject {
+  name: string;
+  value: validMovieOrderColumn;
+}
+
+export type validMovieOrderColumn = "created_at" | "title" | "seen_at";
+
+export const moviesOrderColumnsArray: validMovieOrderColumnObject[] = [
+  {
+    name: "Creation Date",
+    value: "created_at",
+  },
+  {
+    name: "Name",
+    value: "title",
+  },
+  {
+    name: "Watched Date",
+    value: "seen_at",
+  },
+];
+
+export type validMovieOrderBy = "asc" | "desc";
